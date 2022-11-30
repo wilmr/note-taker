d7c22172b26963c6875a077eefd3576597a05bc3
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-11-29 18:41:28 -0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Note Taker Starter Code
+
+## Table of Contents
+
+- [Description](#description)
+- [Usage](#usage)
+
+## Preview
+
+![test note](https://user-images.githubusercontent.com/50350162/204694355-0142a6c9-8f5b-4ce7-8afc-b94ae98494bd.gif)
+
+## Description
+
+This is a simple note taking app. Enter a Title and then the body of the note and click the 'save' icon to save it.
+
+## Usage
+
+To start application, type 'npm start' into the terminal.
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 
+// importing functions from noteMods
 const { createNote, deleteNote } = require('../../lib/noteMods');
+// using json db to store notes
 let { notesArr } = require('../../db/db.json');
+
+// get route
 
 router.get('/notes', (req, res) => {
 	let results = notesArr;
 	res.json(results);
 });
 
+// post route
 router.post('/notes', (req, res) => {
 	if (notesArr) {
 		req.body.id = notesArr.length.toString();
@@ -16,6 +21,8 @@ router.post('/notes', (req, res) => {
 	}
 	res.json(createNote(req.body, notesArr));
 });
+
+// delete route
 
 router.delete('/notes/:id', async (req, res) => {
 	const { id } = req.params;
