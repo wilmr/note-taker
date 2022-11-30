a5c173ee19c5d921c5f0d04069f2ddb2edb5dd3d
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-11-29 17:13:22 -0800

    add express setup to start server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Parse URL encoded & JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Host public folder
+app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+	console.log(`API server now on port ${PORT}!`);
+});
